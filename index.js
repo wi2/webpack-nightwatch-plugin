43504784c2e20e7ce74d4f3b775d9ba396550983
Mateus Miranda <info.mateusmiranda@gmail.com>
2018-03-24 13:26:20 -0300

    leave the folder lib and the folder webpack-nightwatch-plugin production mode

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import path from 'path'
-import {exec, spawnSync} from 'child_process'
+import fs from 'fs'
+import { exec, spawnSync } from 'child_process'
 
 export default class WebpackNightWatchPlugin {
 
@@ -17,11 +18,17 @@ export default class WebpackNightWatchPlugin {
     })
 
     compiler.plugin(this.options.onEmit ? 'emit' : 'done', (compilation, callback) => {
-      const env = Object.assign({}, process.env, {LANG: 'en_US.UTF-8'})
-      const nightwatch = exec(path.join(__dirname, '../node_modules/.bin/nightwatch'), [
+      const env = Object.assign({}, process.env, { LANG: 'en_US.UTF-8' });
+
+      var nightwatchPath = path.join(__dirname, '../node_modules/.bin/nightwatch');
+      if (!fs.existsSync(nightwatchPath)) {
+        // leave the folder "lib" and the folder "webpack-nightwatch-plugin"
+        nightwatchPath = path.join(__dirname, '../../.bin/nightwatch');
+      }
+      var nightwatch = exec(nightwatchPath, [
         '-c',
         this.options.url
-      ], {env})
+      ], { env });
 
       nightwatch.stdout.on('data', data => {
         process.stdout.write(data.toString())
